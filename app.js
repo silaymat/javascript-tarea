86c6c82c5eafdd4d5df1ac8203d9edf573e0bf8a
silvia Aymat <silaymat@yahoo.com.ar>
2022-03-23 00:38:37 -0300

    Primera tarea de javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+ //codigo de javascript
+      // alert("bienvenidos a javascript");
+
+      //mensaje en consola
+      console.log("bienvenidos a javascript");
+      console.log(5 + 3);
+
+      //mostrar en el navegador
+     //document.write("Es un gusto saludarte en este curso");
