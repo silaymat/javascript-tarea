13931e7bf0629b698652c6cff9a38dd11da52574
silvia Aymat <silaymat@yahoo.com.ar>
2022-04-17 22:26:35 -0300

    agrego tarea del 18-04-22

---
+++ tareas-JS/18-04-22.js
@@ -0,0 +1,5 @@
+let edad = parseInt (prompt("Pon tu edad"));
+let nombre = prompt("Esribe tu nombre");
+let edadFinal = 1;
+let result = (edad) + (edadFinal)
+console.log(`Hola ${nombre} tienes ${edad} años y el año que viene tendrás ${result}`);
